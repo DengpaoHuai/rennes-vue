5911986f44511acfcb7b493dc227dd52df010f0e
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-06-06 11:43:22 +0200

    init

---
+++ src/components/DemoComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+type Props = {
+    title: string;
+};
+
+const { title } = defineProps<Props>();
+
+</script>
+
+
+<template>
+    <slot name="header"></slot>
+    <h1>
+        {{ title }}
+    </h1>
+    <slot></slot>
+</template>
